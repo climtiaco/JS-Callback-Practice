818019db470331cc060ada0b8cc54f84f9dee4b2
Christian Limtiaco <114689190+climtiaco@users.noreply.github.com>
2023-01-03 23:40:25 -0800

    Finalized Project

--- move.js
+++ move.js
@@ -1,3 +1,4 @@
+
 function move(element) {
     element.style.position = 'fixed'
 
@@ -6,8 +7,55 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    function moveWithArrowKeys(left, bottom){
-
+    function moveWithArrowKeys(left, bottom, handleDirectionChange){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x = x - 1
+            }
+            if(direction === 'north'){
+                y = y + 1
+            }
+            if(direction === 'east'){
+                x = x + 1
+            }
+            if(direction === 'south'){
+                y = y - 1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            handleDirectionChange(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            handleDirectionChange(direction)
+        })
     }
 
     return {
@@ -26,16 +74,16 @@ function moveWithArrowKeys(left, bottom, handleDirectionChange){
     
     function moveCharacter(){ 
         if(direction === 'west'){
-            x-=1
+            x = x - 1
         }
         if(direction === 'north'){
-            y+=1
+            y = y + 1
         }
         if(direction === 'east'){
-            x+=1
+            x = x + 1
         }
         if(direction === 'south'){
-            y-=1
+            y = y - 1
         }
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
@@ -66,3 +114,4 @@ function moveWithArrowKeys(left, bottom, handleDirectionChange){
         handleDirectionChange(direction)
     })
 }
+
